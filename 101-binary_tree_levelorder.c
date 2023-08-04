93ad7f97d0f10e41a5569c82133c250d028d97aa
fmadala@hotmail.com <fmadala@hotmail.com>
2023-08-04 12:32:41 -0700

    101-binary_tree_levelorder.c

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using level-order
+ * traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func:  pointer to a function to call for each node. The value in the node
+ * -must be passed as a parameter to this function
+ * Return: If tree or func is NULL, do nothing
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t level, maxlevel;
+
+	if (!tree || !func)
+		return;
+
+	maxlevel = binary_tree_height(tree) + 1;
+
+	for (level = 1; level <= maxlevel; level++)
+		btlo_helper(tree, func, level);
+}
+
+/**
+ * btlo_helper - traverse a binary tree in a Bottom-to-Top, Left-to-Right
+ * -(BTLO) order up to a specific level and apply a given function to each
+ * -node's value
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ * @level: level of tree to call func upon
+ */
+
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		btlo_helper(tree->left, func, level - 1);
+		btlo_helper(tree->right, func, level - 1);
+	}
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: tree is NULL, else return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
